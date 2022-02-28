1d84b63d6e1c2614b594c93e35a39214f99e9586
Pedro Paulo Monteiro Muniz Barbosa <pedropaulommb@gmail.com>
2022-02-28 19:57:46 -0300

    Add changes on the front-end

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def homepage():
-    return render_template("homepage.html")
+def index():
+    return render_template("index.html")
 
 
 @app.route("/contacts")
