65828a7dce544ec7114af4ea1bec26128db8e839
Pedro Paulo Monteiro Muniz Barbosa <pedropaulommb@gmail.com>
2022-03-01 14:11:21 -0300

    Add the Skills section

--- static/script.js
+++ static/script.js
@@ -1,11 +1,24 @@
 /* Open and close the menu on mobile mode */
 
-const menuMobile = document.querySelector('.menu-mobile')
-const body = document.querySelector('body')
+const menuMobile = document.querySelector(".menu-mobile");
+const body = document.querySelector("body");
 
-menuMobile = addEventListener('click', () => {
-    menuMobile.classList.contains("bi-list")
-        ? menuMobile.classList.replace("bi-list", "bi-x")
-        : menuMobile.classList.replace("bi-x", "bi-list");
-    body.classList.toggle("menu-nav-active")
-})
+menuMobile.addEventListener("click", () => {
+  menuMobile.classList.contains("bi-list")
+    ? menuMobile.classList.replace("bi-list", "bi-x")
+    : menuMobile.classList.replace("bi-x", "bi-list");
+  body.classList.toggle("menu-nav-active");
+});
+
+/* Close the menu when click on any item and change the icon to list */
+
+const navItem = document.querySelectorAll(".nav-item");
+
+navItem.forEach((item) => {
+  item.addEventListener("click", () => {
+    if (body.classList.contains("menu-nav-active")) {
+      body.classList.remove("menu-nav-active");
+      menuMobile.classList.replace("bi-x", "bi-list");
+    }
+  });
+});
